93a00b66c0b2be39032f9d99999d9ad8e2c786c7
Gal Ifrah <gal.ifrahh@gmail.com>
2018-06-06 07:33:47 +0300

    create first Test class

--- src/test/java/VroomAutomation/VroomProject/HomePageTests.java
+++ src/test/java/VroomAutomation/VroomProject/HomePageTests.java
@@ -1,12 +1,80 @@
 package VroomAutomation.VroomProject;
+import org.junit.Assert;
+
+import org.junit.Ignore;
 
 import org.junit.Test;
 
-public class HomePageTests {
-	
+import VroomAutomation.VroomProject.Basic;
+
+import VroomAutomation.VroomProject.Locators;
+
+import VroomAutomation.VroomProject.TestParams;
+
+import VroomAutomation.VroomProject.PageObjects.BuyPage;
+
+import VroomAutomation.VroomProject.PageObjects.vroomHomePage;
+
+public class HomePageTests extends Basic {
+
+	@TestParams(testDescription = "search", testTimeout = 600)
+
 	@Test
-	public void test1(){
-		
+
+	public void SearchTest() {
+
+		vroomHomePage.openHomePage();
+
+		vroomHomePage.textToSearch("volvo");
+
+		vroomHomePage.clickSearchButton();
+
+		Assert.assertEquals("Volvo",
+
+				webDriver.getElementBy(Locators.XPATH, "//a[@class = 'active-filters-section']").getText());
+
 	}
-	
+
+	@Ignore
+
+	@Test
+
+	public void highestPrice() throws InterruptedException {
+
+		vroomHomePage.openHomePage();
+
+		vroomHomePage.clickOnBuyButton();
+
+		BuyPage.sortByHighestPrice();
+
+	}
+
+	@Ignore
+
+	@Test
+
+	public void clickOnFortune() {
+
+		vroomHomePage.openHomePage();
+
+		vroomHomePage.clickOnFortuneButton();
+
+		String fortuneText = vroomHomePage.getFortuneText();
+
+		Assert.assertEquals(
+
+				"\"Vroom wants to make buying or selling a used car as quick and painless as ordering an Uber.\"",
+
+				fortuneText);
+
+		System.out.println(fortuneText);
+
+	}
+
+	@Test
+
+	public void StamTest() {
+
+		System.out.println("great!!!");
+	}
 }
